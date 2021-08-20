6e4298fd5722f186531711eb718c45cfbc1d8944
Lab360-inc <76831380+Lab360-inc@users.noreply.github.com>
2021-08-20 15:36:10 -0400

    Update flutter_fb_news.dart

--- lib/flutter_fb_news.dart
+++ lib/flutter_fb_news.dart
@@ -123,17 +123,6 @@ CarouselSlider( items: FbNewsFeed(
                           options: CarouselOptions(
       height: 400,
       aspectRatio: 16/9,
-      viewportFraction: 0.8,
-      initialPage: 0,
-      enableInfiniteScroll: true,
-      reverse: false,
-      autoPlay: true,
-      autoPlayInterval: Duration(seconds: 3),
-      autoPlayAnimationDuration: Duration(milliseconds: 800),
-      autoPlayCurve: Curves.fastOutSlowIn,
-      enlargeCenterPage: true,
-      onPageChanged: callbackFunction,
-      scrollDirection: Axis.horizontal,
    ));
                       }
                     },
